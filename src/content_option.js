c32cba0ee8ecadfb3633caff353cf83f57e6a6e1
Anselm <r2nd03-dude@protonmail.com>
2022-10-12 14:16:27 +0500

    updated contact form with working email services

--- src/content_option.js
+++ src/content_option.js
@@ -136,14 +136,14 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
+    YOUR_EMAIL: "falcon9@duck.com",
+    YOUR_FONE: "No Number",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: "service_2uadzai",
+    YOUR_TEMPLATE_ID: "template_qa7yzvi",
+    YOUR_USER_ID: "_falcon9",
 };
 
 const socialprofils = {
